0949d359c6cae90ceb6f15ad9fa1b0526464f9d7
yc <850020922@qq.com>
2025-08-25 18:50:00 +0800

    install docker

--- Docker_insatll.sh
+++ Docker_insatll.sh
@@ -1,3 +1,5 @@
+#运行以下命令卸载所有有冲突的软件包：
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl
@@ -14,3 +16,31 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+#可以换成阿里云的
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.aliyun.com/docker-ce/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  sudo apt-get update
+
+  #进行安装
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  #安装好后，可以通过systemctl status docker查看状态，但是现在使用的还是默认镜像源，需要配置下
+  #进入阿里云官网https://cr.console.aliyun.com/ 登录后，在左侧菜单找到 “镜像加速器”（路径：容器镜像服务 → 镜像工具 → 镜像加速器）页面中会显示你的 专属加速地址
+  #您可以通过修改daemon配置文件/etc/docker/daemon.json来使用加速器
+
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://你的专属加速地址.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+#在通过运行hello-world镜像来验证安装是否成功：
+ sudo docker run hello-world
+
+ #成功安装并启动 Docker 引擎
